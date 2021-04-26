3e03b602f35cb0e0a9442cc542caecc79d0d7fa7
Martin Hoppe <martin.hoppe@intis.de>
2021-04-26 07:12:13 +0200

    implementation completed

--- standard/reportengine/reportblock/functionalbreakdown.js
+++ standard/reportengine/reportblock/functionalbreakdown.js
@@ -1,54 +1,89 @@
 /* reposyd-reportblock-header */
-const Diagrams = require( 'reposyd-core' ).ReportFramework.Diagrams
-const LexicographicalOrder = require( 'reposyd-core' ).Utils.LexicographicalOrder
-const ReportBlock = require( 'reposyd-core' ).ReportFramework.ReportBlock
-const ReportTable = require( 'reposyd-core' ).ReportFramework.ReportTable
-const Rule = require( 'js-rules-engine' ).Rule
-const Utils = require( 'reposyd-core' ).Utils
+const ReportBlock = require('ReportFramework').ReportBlock
+const ReportTable = require('ReportFramework').ReportTable
+const Utils = require('Utils')
 /* reposyd-reportblock-header */
 class ReportBlockFunctionalBreakdown extends ReportBlock {
-  constructor( section, engine ) {
-    super( section, engine )
+  constructor(section, engine) {
+    super(section, engine)
     /* reposyd-reportblock-constructor */
-    this.sectionNumber = new Utils.LexicographicalOrder.SectionNumber( this.section.number )
+    this.sectionNumber = new Utils.LexicographicalOrder.SectionNumber(this.section.number)
     /* reposyd-reportblock-constructor */
+    this.translations = {
+      /* reposyd-reportblock-translations */
+      de: {
+        description: 'Beschreibung',
+        in: 'Eingabe',
+        out: 'Ausgabe',
+        title: 'Titel',
+        type: 'Typ'
+      },
+      en: {
+        description: 'Description',
+        in: 'Input',
+        out: 'Output',
+        title: 'Title',
+        type: 'Type'
+      }
+      /* reposyd-reportblock-translations */
+    }
   }
   /* reposyd-reportblock-body */
-  renderContent() {
+  renderContent () {
     const self = this
-    this.format.writeHtml( this.section.text )
+    this.format.writeHtml(this.section.text, 'description')
 
-    const fn = this.designData.designDataObject( this.section.reportblock.options.reportblock.startingpoint )
-    if ( !fn ) {
+    const fn = this.designData.designDataObject(this.section.reportblock.options.reportblock.startingpoint)
+    if (!fn) {
 
     } else {
       self.sectionNumber.add()
-      followBreakdown( fn )
+      followBreakdown(fn)
     }
 
-    function followBreakdown( fn ) {
-      self.addSection( self.sectionNumber.toString(), fn.attributes.title )
-      self.designData.modeling.object.diagrams(fn.uuid, self.section.reportblock.options.reportblock.diagramtype).forEach(dm => {
-        self.writeDiagram( dm )
-      })
-      self.writeHtml( fn.attributes.description, 'description' )
+    function followBreakdown (fn) {
+      self.addSection(self.sectionNumber.toString(), fn.attributes.title)
+      if (self.section.reportblock.options.reportblock.diagramtype !== 'none') {
+        self.designData.modeling.object.diagrams(fn.uuid, self.section.reportblock.options.reportblock.diagramtype).forEach(dm => {
+          self.writeDiagram(dm)
+        })
+      }
+      self.writeHtml(fn.attributes.description, 'description')
+      let interfaceItems = self.designData.designDataLinks(fn, 'hasoutputs', 'II').map(x => { return { dir: 'out', ddo: x.ddo } })
+      interfaceItems = interfaceItems.concat(self.designData.designDataLinks(fn, 'hasinputs', 'II').map(x => { return { dir: 'in', ddo: x.ddo } }))
+      if (interfaceItems.length > 0) {
+        const table = new ReportTable.Table(0, 3)
+        table.horizontalHeader.setStyle(-1, 'tableHeader')
+        table.horizontalHeader.addRow()
+        table.horizontalHeader.setCaption(0, this.tc('titel'))
+        table.horizontalHeader.setCaption(1, this.tc('typ'))
+        table.horizontalHeader.setWidth(1, 20)
+        table.horizontalHeader.setCaption(2, this.tc('description'))
+        interfaceItems.forEach(item => {
+          const row = table.appendRow()
+          row.cell(0).setData(item.ddo.attributes.title)
+          row.cell(1).setData(item.dir === 'in' ? this.tc('in') : this.tc('out'))
+          row.cell(2).setData(`<html><body>${item.ddo.attributes.description}</body></html>`)
+        })
+        self.writeTable(table)
+      }
 
       self.sectionNumber.add()
-      self.designData.designDataLinks( fn, 'dividedinto', 'FN' ).forEach( item => {
+      self.designData.designDataLinks(fn, 'dividedinto', 'FN').forEach(item => {
         followBreakdown(item.ddo)
-      } )
+      })
 
       self.sectionNumber.remove()
-      
+
       self.sectionNumber.inc()
     }
   }
   /* reposyd-reportblock-body */
-  static RuleDefinition( settings ) {
-    var rule = ReportBlockRequirements.DefaultSettings( settings ).rule;
+  static RuleDefinition (settings) {
+    var rule = ReportBlockRequirements.DefaultSettings(settings).rule;
     return rule === undefined ? null : rule
   }
-  static DefaultSettings( settings ) {
+  static DefaultSettings (settings) {
     return {
       /* reposyd-reportblock-settings */
       section: [{
